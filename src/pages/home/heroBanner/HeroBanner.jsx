c076db69cbb55b96b47d136fc20a718cb1b70f03
avinash hubale <avinashhubale1@gmail.com>
2023-06-10 22:32:45 +0530

    Homepage styling

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -34,19 +34,20 @@ const HeroBanner = () => {
 
       <div className="opacity-layer"></div>
       <ContentWrapper>
-        <div className="heroBannerContent"></div>
-        <span className="title">Welcome.</span>
-        <span className="subTitle">
-          Millions of movies, TV shows and people to discover. Explore now.
-        </span>
-        <div className="searchInput">
-          <input
-            type="text"
-            placeholder="Search for a movie or tv show...."
-            onChange={(e) => setQuery(e.target.value)}
-            onKeyUp={searchQueryHandler}
-          />
-          <button>Search</button>
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show...."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
         </div>
       </ContentWrapper>
     </div>
