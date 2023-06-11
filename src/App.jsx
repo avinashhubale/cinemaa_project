00eaaaf7d237870b8bc538dc7af4185a255222fe
avinash hubale <avinashhubale1@gmail.com>
2023-06-11 11:53:22 +0530

    Header section designed

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      {/* <Header/> */}
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:media/:id" element={<Details />} />
