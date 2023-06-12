fc46f045a03c0343508f8a2f4c77d05b9a4189ac
avinash hubale <avinashhubale1@gmail.com>
2023-06-12 12:24:57 +0530

    main carousel component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import './style.scss'
-import HeroBanner from './heroBanner/HeroBanner'
-import Trending from './trending/Trending'
+import React from "react";
+import "./style.scss";
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 const Home = () => {
   return (
-    <div className='homePage'>
-        <HeroBanner/>
-        <Trending/>
-        <div style={{height:1000}}></div>
+    <div className="homePage">
+      <HeroBanner />
+      <Trending />
+      <div style={{ height: 1000 }}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
