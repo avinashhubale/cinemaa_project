f61aae34d46a300d9f313e39674acde08eac92d7
avinash hubale <avinashhubale1@gmail.com>
2023-06-14 16:50:06 +0530

    Details page top cast and videos section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,6 +4,8 @@ import "./style.scss";
 
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideosSection  from "./videosSection/VideosSection";
 
 const Details = () => {
   const { mediaType, id } = useParams();
@@ -14,6 +16,8 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data = {credits?.cast} loading={creditsLoading}/>
+      <VideosSection data={data} loading={loading} />
     </div>
   );
 };
